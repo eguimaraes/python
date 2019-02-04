23fed18c5746a0e6b16f9a5330e8b21c5f50fb31
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-04 04:17:38 -0200

    Update util.py

--- util.py
+++ util.py
@@ -1,3 +1,11 @@
 def printTime(msg):
     x = datetime.datetime.now()
     print(msg+str(x))
+
+def writeDsk(arq,msg):
+    x = datetime.datetime.now()
+    msg2w=msg+str(x)
+    print(msg2w)
+    filehandle = open(arq,'a')  
+    filehandle.write(msg2w) 
+    filehandle.close()  
