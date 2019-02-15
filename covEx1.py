66bbb78aadc40035dd69420dc90ad1b893853c44
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-02-14 22:08:13 -0200

    Create covEx1.py

---
+++ covEx1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+plt.style.use('ggplot')
+plt.rcParams['figure.figsize'] = (12, 8)
+
+# Normal distributed x and y vector with mean 0 and standard deviation 1
+x = np.random.normal(0, 1, 500)
+y = np.random.normal(0, 1, 500)
+X = np.vstack((x, y)).T
+
+plt.scatter(X[:, 0], X[:, 1])
+plt.title('Generated Data')
+plt.axis('equal');
