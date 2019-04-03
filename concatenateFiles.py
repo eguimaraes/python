4b06fca6d77de0704cb1de68e3a4c10f11c74544
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-04-03 16:08:14 -0300

    Create concatenateFiles.py

---
+++ concatenateFiles.py
@@ -0,0 +1,24 @@
+import numpy as np
+from io import StringIO 
+
+def readBellTxt(prefix,i):         
+    matrixBell=np.loadtxt(prefix+str(i)+".txt")
+    return matrixBell
+  
+
+def getBell(prefix,start,end):
+    sizeRow=31
+    sizeCollun=500
+    matrix=np.zeros((sizeRow,sizeCollun),dtype=float)
+    print(matrix.size)
+    print(matrix.shape)
+    for i in range(start,end):
+        matrixActual=readBellTxt("xxx",i)
+        for j in range(sizeRow):
+            print(str(j)+","+str(i))
+            
+            matrix[j,i]=matrixActual[j]
+    np.savetxt("matrixB.txt",matrix)
+     
+    
+
