b5e31083838b90bfe6bafd24c5a1994d39718016
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2019-04-25 17:45:44 -0300

    Create contourEx1.py

---
+++ contourEx1.py
@@ -0,0 +1,9 @@
+import matplotlib 
+import numpy as np
+xlist = np.linspace(-3.0, 3.0, 3)
+ylist = np.linspace(-3.0, 3.0, 4)
+X, Y = np.meshgrid(xlist, ylist)
+print(xlist)
+print(ylist)
+print(X)
+print(Y)
