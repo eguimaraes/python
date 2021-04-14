fb2cf197df96ffbb404b6a6cfe0dce5d41be3985
Edvaldo Guimarães <edvaldoguimaraes2013@hotmail.com>
2021-04-14 19:42:51 -0300

    Create argsParse.py

---
+++ argsParse.py
@@ -0,0 +1,6 @@
+#https://docs.python.org/3/howto/argparse.html
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("square", help="display a square of a given number", type=int)
+args = parser.parse_args()
+print(args.square**2)
